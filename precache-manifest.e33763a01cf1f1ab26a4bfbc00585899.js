bc0b7d5dea0fdaebbb64f32eb9b13bbf2b67b56e
KushwahaVikashJ <kushwahavicky743@gmail.com>
2020-07-09 13:49:26 +0530

    Updates

---
+++ precache-manifest.e33763a01cf1f1ab26a4bfbc00585899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d50abac346b641f05d4d7b30d040bf",
+    "url": "/MyBurger/index.html"
+  },
+  {
+    "revision": "29c334eb864dfc68c270",
+    "url": "/MyBurger/static/css/main.b78a0313.chunk.css"
+  },
+  {
+    "revision": "4f5f8df5fa053d3827b4",
+    "url": "/MyBurger/static/js/2.ec104039.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyBurger/static/js/2.ec104039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29c334eb864dfc68c270",
+    "url": "/MyBurger/static/js/main.ef707f89.chunk.js"
+  },
+  {
+    "revision": "0490de9f0fd5286965c2",
+    "url": "/MyBurger/static/js/runtime-main.b2d0726e.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/MyBurger/static/media/burger-logo.b8503d26.png"
+  }
+]);
